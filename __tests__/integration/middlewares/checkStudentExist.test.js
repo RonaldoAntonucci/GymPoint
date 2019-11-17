005d280e8aabd48d95cb7992d83eff430a198245
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2019-11-17 18:32:38 -0300

    test checkstudent middleware

--- __tests__/integration/middlewares/checkStudentExist.test.js
+++ __tests__/integration/middlewares/checkStudentExist.test.js
@@ -1,33 +1,32 @@
 import request from 'supertest';
+import express from 'express';
 import factory from '../../factories';
 import truncate from '../../util/truncate';
 
-import app from '../../../src/app';
 import studentExist from '../../../src/app/middlewares/checkStudentExist';
 
-app.post('/testStudentExist', studentExist, (req, res) => {
+const app = express();
+app.post('/testStudentExist/:studentId', studentExist, (req, res) => {
   res.json({ studentId: req.studentId });
 });
 
 describe('middleware checkStudentExist', () => {
-  beforeEach(async () => {
-    await truncate();
-  });
   it('should be call middleware with valid student and return studentId', async () => {
     const { id } = await factory.create('Student');
 
     const res = await request(app)
-      .post(`./testStudentExist/${id}`)
+      .post(`/testStudentExist/${id}`)
       .send({});
 
     expect(res.status).toBe(200);
-    expect(res.body.studentId).toBe(id);
+    expect(res.body.studentId).toBe(`${id}`);
+    await truncate();
   });
 
   it('should be call middleware checkStudentExist with invalid student and return an error', async () => {
     const id = -1;
     const res = await request(app)
-      .post(`./testStudentExist/${id}`)
+      .post(`/testStudentExist/${id}`)
       .send({});
 
     expect(res.status).toBe(400);
@@ -36,7 +35,7 @@ describe('middleware checkStudentExist', () => {
 
   it('should be call middleware checkStudentExist without student', async () => {
     const res = await request(app)
-      .post(`./testStudentExist/`)
+      .post(`/testStudentExist/${undefined}`)
       .send({});
 
     expect(res.status).toBe(400);
