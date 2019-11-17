571d7a1feb0ca19a46fbc3b983bfdb17466728dd
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2019-11-17 15:46:51 -0300

    mock do redis configurado

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,4 +1,11 @@
-export default {
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-};
+import '../bootstrap';
+import redisMock from 'redis-mock';
+
+const redis =
+  process.env.NODE_ENV === 'test'
+    ? redisMock.createClient()
+    : {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+      };
+export default redis;
